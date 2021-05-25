a688d64ba268429423dc7f3a24046fa2e0e85e65
Nazryl Lim <nazryl@live.com>
2021-05-25 17:15:28 +0800

    First commit

---
+++ src/components/insertData.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+
+const newHero = {
+  name: "White Adam",
+  age: 30,
+  secretIdentity: "The Rock",
+  powers: ["God", "Free money", "Light"],
+};
+
+class InsertData extends Component {
+  constructor() {
+    super();
+    this.insertNewHero = this.insertNewHero.bind(this);
+    this.state = {
+      status: null,
+    };
+  }
+
+  // Main
+  render() {
+    return (
+      <div style={this.center}>
+        <button
+          onClick={this.insertNewHero}
+          className="btn btn-secondary btn-sm m-4"
+        >
+          Insert Hero
+        </button>
+        <span className={this.getBadgesClasses()}>{this.displayStatus()}</span>
+      </div>
+    );
+  }
+
+  // Functions
+  insertNewHero = () => {
+    const encodeFormData = (data) => {
+      return Object.keys(data)
+        .map(
+          (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+      body: encodeFormData({
+        text: JSON.stringify(newHero),
+        command: "/heroes",
+      }),
+    };
+
+    fetch(
+      "https://4cate66olg.execute-api.ap-southeast-1.amazonaws.com/default/time-tracking-server",
+      requestOptions
+    )
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((json) => {
+            throw json;
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ status: JSON.stringify(data, null, 4) });
+        console.log("Data Inserted: " + data.name);
+      })
+      .then((json) => console.log(json))
+      .catch((err) => {
+        this.setState({ status: err.message });
+        console.log("Catch: ", err);
+      });
+  };
+
+  displayStatus() {
+    const { status } = this.state;
+    return status === null ? "No status" : status;
+  }
+
+  // CSS
+  center = {
+    textAlign: "center",
+    fontWeight: "bold",
+  };
+
+  // Bootstrap
+  getBadgesClasses() {
+    let classes = "badge bg-";
+    if (this.state.status === null) {
+      classes += "warning";
+    } else if (this.state.status === "Failed to fetch") {
+      classes += "danger";
+    } else {
+      classes += "primary";
+    }
+    return classes;
+  }
+}
+
+export default InsertData;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import InsertData from './components/insertData';
+import ReadData from './components/readData';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('title'));
+ReactDOM.render(<InsertData />, document.getElementById('insert'));
+ReactDOM.render(<ReadData />, document.getElementById('read'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
