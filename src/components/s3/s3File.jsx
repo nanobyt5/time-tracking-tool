95f6dee18bb9550fff3e2596b2752624e7eac78f
marcus <markuz5116@gmail.com>
2021-07-12 11:01:02 +0800

    Grab file from s3 bucket and convert to json

--- src/components/s3/s3File.jsx
+++ src/components/s3/s3File.jsx
@@ -19,6 +19,38 @@ const s3Params = {
   Prefix: "",
 };
 
+const processData = dataString => {
+  const dataStringLines = dataString.split(/\r\n|\n/);
+  const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+
+  const list = [];
+  for (let i = 1; i < dataStringLines.length; i++) {
+    const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+    if (headers && row.length === headers.length) {
+      const obj = {};
+      for (let j = 0; j < headers.length; j++) {
+        let d = row[j];
+        if (d.length > 0) {
+          if (d[0] === '"')
+            d = d.substring(1, d.length - 1);
+          if (d[d.length - 1] === '"')
+            d = d.substring(d.length - 2, 1);
+        }
+        if (headers[j]) {
+          obj[headers[j]] = d;
+        }
+      }
+
+      // remove the blank rows
+      if (Object.values(obj).filter(x => x).length > 0) {
+        list.push(obj);
+      }
+    }
+  }
+
+  return list;
+}
+
 class S3File extends Component {
   constructor() {
     super();
@@ -61,6 +93,8 @@ class S3File extends Component {
   async exportFromS3(params) {
     s3.getObject(params, function (err, data) {
       if (data) {
+        let content = data.Body.toString('utf-8');
+        console.log("content", processData(content));
         var filename = params.Key;
         var blob = new Blob([data.Body], {
           type: "",
